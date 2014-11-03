a983af5e658b27f452e44d8de5209b41e7559aee
Mickael Vieira <contact@mickael-vieira.com>
2014-11-03 01:39:35 +0000

    Removed comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-    // Project configuration.
     grunt.initConfig({
-        // Metadata.
+
         pkg: grunt.file.readJSON('package.json'),
+
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-        // Task configuration.
+
         concat: {
             options: {
                 banner: '<%= banner %>',
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
                 tasks: ['jshint:gruntfile']
             },
             css: {
-               files: 'css/**/*.css',
+                files: 'css/**/*.css',
                 tasks: ['buildcss']
             },
             js: {
