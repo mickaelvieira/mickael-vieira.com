fa2fe3be983bb2b6ee1eb9fdf76900347a723fe1
Mickael Vieira <contact@mickael-vieira.com>
2015-12-05 17:05:43 +0000

    Add PGP link

---
+++ gulp/bust.js
@@ -0,0 +1,23 @@
+"use strict";
+//
+//var getRoot = require('./get-root-dir');
+//
+//var gulp = require('gulp');
+//var bust = require('gulp-buster');
+//var config = require('config');
+//
+//var fileName = config.get('gulp.cacheBuster.fileName');
+//var distPath = getRoot() + config.get('paths.dist');
+//var stylesPath = getRoot() + config.get('paths.styles.dest');
+//var scriptsPath = getRoot() + config.get('paths.scripts.dest');
+
+module.exports = function( cb ) {
+    //return gulp.src( [
+    //    scriptsPath + '**/*.js',
+    //    stylesPath + '**/*.css'
+    //] )
+    //.pipe( bust( {
+    //    fileName: fileName
+    //} ) )
+    //.pipe( gulp.dest( distPath ) );
+};
