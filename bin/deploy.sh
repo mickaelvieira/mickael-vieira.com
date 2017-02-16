30b3c78f22c6ff5f8ee9dd680a4670a03c3ce2ad
Mickael Vieira <contact@mickael-vieira.com>
2017-02-16 11:46:28 +0000

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-set -o nounset # set -u
-set -o errexit # set -e
-set -o pipefail
 #set -x
+set -u -e -o pipefail
 
-declare -r RESET_ALL="\x1b[0m"
-declare -r BG_RED="\x1b[41m"
-declare -r BG_BLUE="\x1b[44m"
-declare -r BG_MAGENTA="\x1b[45m"
-declare -r FG_WHITE="\x1b[97m"
-declare -r FG_RED="\x1b[31m"
-declare -r FG_BLUE="\x1b[34m"
-declare -r FG_MAGENTA="\x1b[35m"
+info() {
+    echo -e "\x1b[32m\xE2\x87\x92 $1 \x1b[0m"
+}
 
 error() {
-    echo -e "${BG_RED}${FG_WHITE}ERROR${RESET_ALL}  ${FG_RED}$1${RESET_ALL}" >&2
-}
-info() {
-    echo -e "${BG_BLUE}${FG_WHITE}INFO${RESET_ALL}  ${FG_BLUE}$1${RESET_ALL}"
+    echo -e "\x1b[97m\x1b[41m\x21\x1b[0m \x1b[31m$1\x1b[0m" 1>&2
+    exit 1
 }
 
-declare -r base=${DEV_DIR:-}
+declare -r BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)"
 
-if [[ -z "${base}" ]]; then
+if [[ -z "${BASE}" ]]; then
     error "Base directory cannot be an empty string"
     exit 1
 fi
 
-declare -r src="${base}/mickael-vieira.com/"
-declare -r dest="${base}/mickaelvieira.github.io/"
+declare -r SRC="${BASE}/mickael-vieira.com/"
+declare -r DEST="${BASE}/mickaelvieira.github.io/"
 
-info "Running build"
-npm run build
+[[ -d $SRC ]] || error "Source directory ${SRC} does not exist"
+[[ -d $DEST ]] || error "Destination directory ${DEST} does not exist"
 
-info "Synchronizing files"
+update_source() {
+    info "Updating source"
 
-rsync --verbose \
+    cd "${SRC}"
+
+    git pull
+}
+
+update_destination() {
+    info "Updating destination"
+
+    cd "${DEST}"
+
+    git pull
+}
+
+sync_files() {
+    info "Synchronizing files"
+
+    cd "${SRC}"
+
+    rsync --verbose \
         --times \
         --owner \
         --group \
@@ -44,17 +54,37 @@ rsync --verbose \
         --delete-before \
         --include="index.html" \
         --include="dist/***" \
-        --exclude="*" "${src}" "${dest}"
+        --exclude="*" "${SRC}" "${DEST}"
+}
 
-cd "${dest}"
+build_source() {
+    info "Running build"
 
-info "Moved to $(pwd)"
+    cd "${SRC}"
 
-info "Auto-committing build result and pushing to master"
+    npm install
+}
 
-git add -A .
-git commit -m "Auto-committing build result"
-git push origin master
+auto_commit() {
+    info "Auto-committing build result and pushing to master"
 
-info "Done!"
-exit 0
+    cd "${DEST}"
+
+    git add -A .
+    git commit -m "Auto-committing build result"
+    git push origin master
+}
+
+main() {
+    update_source
+    update_destination
+    build_source
+    sync_files
+    auto_commit
+
+    info "Done!"
+    exit 0
+}
+
+main
+
